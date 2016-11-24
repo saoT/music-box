840a62eb9593595640852a5535449712c4532ee6
Larisa <larisagrigorut@gmail.com>
2016-11-24 14:19:27 +0100

    add Larisa's music-box

---
+++ Larisa-Grigorut/exoLG.js
@@ -0,0 +1,47 @@
+// yellow rectangle
+var yellowBox = document.getElementById('yellow');
+var audioYe = document.getElementById('audio-ye');
+
+yellowBox.onclick = function(){
+	audioYe.play();
+}
+
+//orange rectangle
+var orangeBox = document.getElementById('orange');
+var audioOr = document.getElementById('audio-or');
+
+orangeBox.onclick = function(){
+	audioOr.play();
+}
+
+//pink rectangle
+var pinkBox = document.getElementById('pink');
+var audioPi = document.getElementById('audio-pi');
+
+pinkBox.onclick = function(){
+	audioPi.play();
+}
+
+//purple rectangle
+var purpleBox = document.getElementById('purple');
+var audioPu = document.getElementById('audio-pu');
+
+purpleBox.onclick = function(){
+	audioPu.play();
+}
+
+//blue rectangle
+var blueBox = document.getElementById('blue');
+var audioBl = document.getElementById('audio-bl');
+
+blueBox.onclick = function(){
+	audioBl.play();
+}
+
+//green rectangle
+var greenBox = document.getElementById('green');
+var audioGr = document.getElementById('audio-gr');
+
+greenBox.onclick = function(){
+	audioGr.play();
+}
