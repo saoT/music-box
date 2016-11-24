09c0c8cbe1725acfd80522e51a17c5e161c015fe
Nasserdine <nasserdine.bouguerne@gmail.com>
2016-11-24 14:41:38 +0100

    nasserdine

---
+++ nasserdine/musicBox.js
@@ -0,0 +1,36 @@
+var p1 = document.getElementById('p1');
+var p2 = document.getElementById('p2');
+var p3 = document.getElementById('p3');
+var p4 = document.getElementById('p4');
+var p5 = document.getElementById('p5');
+var p6 = document.getElementById('p6');
+
+var s1 = document.getElementById('s1');
+var s2 = document.getElementById('s2');
+var s3 = document.getElementById('s3');
+var s4 = document.getElementById('s4');
+var s5 = document.getElementById('s5');
+var s6 = document.getElementById('s6');
+
+p1.onclick = function(){
+	s1.play();
+}
+
+p2.onclick = function(){
+	s2.play();
+}
+
+p3.onclick = function(){
+	s3.play();
+}
+
+p4.onclick = function(){
+	s4.play();
+}
+
+p5.onclick = function(){
+	s5.play();
+}
+p6.onclick = function(){
+	s6.play();
+}
