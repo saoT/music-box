7a6c232c37c661ce20688e129075f4427fb87b59
dounia <d.fergane@gmail.com>
2016-11-24 14:14:11 +0100

    dounia

---
+++ dounia-fergane/exo1/index.js
@@ -0,0 +1,26 @@
+function play1(){
+       var audio = document.getElementById("son1");
+       audio.play();
+}
+function play2(){
+       var audio = document.getElementById("son2");
+       audio.play();
+}
+function play3(){
+       var audio = document.getElementById("son3");
+       audio.play();
+}
+function play4(){
+       var audio = document.getElementById("son4");
+       audio.play();
+}
+function play5(){
+       var audio = document.getElementById("son5");
+       audio.play();
+}
+function play6(){
+       var audio = document.getElementById("son6");
+       audio.play();
+}
+
+
