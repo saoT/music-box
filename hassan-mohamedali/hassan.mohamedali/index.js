60efe06917421fec367958f35ac58d3a000dc6e7
mohamed-ali <hassan.mohamedali75@gmail.com>
2016-11-24 14:42:26 +0100

    hassan

---
+++ hassan-mohamedali/hassan.mohamedali/index.js
@@ -0,0 +1,44 @@
+
+
+
+
+//	var audio = document.getElementByTagName("audio");
+//audio.onclick = function(){
+
+//	audio.play()
+//}
+
+
+$(".orange").onclick(function() {
+	var audio = document.getElementByTagName("audio")[1];
+
+
+	audio.play()
+}
+$(".pink").onclick(function() {
+	var audio = document.getElementByTagName("audio")[2];
+
+
+	audio.play()
+}
+$(".violet").onclick(function() {
+	var audio = document.getElementByTagName("audio")[3];
+
+
+	audio.play()
+}
+$(".blue").onclick(function() {
+	var audio = document.getElementByTagName("audio")[4];
+
+
+	audio.play()
+}
+
+$(".green").onclick(function() {
+	var audio = document.getElementByTagName("audio")[5];
+
+
+	audio.play()
+}
+
+	
