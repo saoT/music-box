b1922434fafc52c025269bfd4362281b6d455fb2
Louis Gaillard <louis.gaillard94@gmail.com>
2016-11-24 14:36:46 +0100

    LouisGaillard

---
+++ LouisGaillard/script.js
@@ -0,0 +1,25 @@
+var a = document.getElementById('a');
+var b = document.getElementById('b');
+var c = document.getElementById('c');
+var d = document.getElementById('d');
+var e = document.getElementById('e');
+var f = document.getElementById('f');
+
+var jaune = document.getElementById('jaune');
+var orange = document.getElementById('orange');
+var rose = document.getElementById('rose');
+var violet = document.getElementById('violet');
+var bleu = document.getElementById('bleu');
+var vert = document.getElementById('vert');
+
+function music (sound){
+  sound.currentTime = 0;
+  sound.play()
+}
+
+  jaune.onclick = music(a);
+  orange.onclick = music(b);
+  rose.onclick = music(c);
+  violet.onclick = music(d);
+  bleu.onclick = music(e);
+  vert.onclick = music(f);
