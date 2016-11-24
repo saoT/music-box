17fdec8eb0d99e595ab9b93e0430cbc22efab962
guillaume <guillaume.bruno93270@gmail.com>
2016-11-24 14:10:48 +0100

    guillaume

---
+++ guillaume-bruno/music.js
@@ -0,0 +1,24 @@
+function play(){
+       var audio = document.getElementById("audio");
+       audio.play();
+              }
+function play2(){
+ var audio = document.getElementById("audio2");
+       audio.play();
+   }
+function play3(){
+ var audio = document.getElementById("audio3");
+       audio.play();
+   }
+function play4(){
+ var audio = document.getElementById("audio4");
+       audio.play();
+   }
+function play5(){
+ var audio = document.getElementById("audio5");
+       audio.play();
+   }
+function play6(){
+ var audio = document.getElementById("audio6");
+       audio.play();
+   }
